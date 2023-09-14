7d61360fd0b930ffee5d2bdbbea69ea46200229b
Igor Eduardo Ramos <139307520+igoreramos@users.noreply.github.com>
2023-09-13 23:17:18 -0300

    jogodeouro

--- script.js
+++ script.js
@@ -1,55 +1,66 @@
-// script.js
 const botaoEntrada = document.querySelector(".botao-entrada");
-const botaoSaida = document.querySelector(".botao-saida");
-const dataHoraAtualSpan = document.getElementById("dataHoraAtual");
-const quemEVoceSelect = document.getElementById("quemEVoce");
-const registrosLista = document.getElementById("registrosLista");
+        const botaoSaida = document.querySelector(".botao-saida");
+        const dataHoraAtualSpan = document.getElementById("dataHoraAtual");
+        const quemEVoceSelect = document.getElementById("quemEVoce");
+        const registrosLista = document.getElementById("registrosLista");
 
-botaoEntrada.addEventListener("click", () => {
-    marcarPonto("Entrada");
-});
+        botaoEntrada.addEventListener("click", () => {
+            marcarPonto("Entrada");
+        });
 
-botaoSaida.addEventListener("click", () => {
-    marcarPonto("Saída");
-});
+        botaoSaida.addEventListener("click", () => {
+            marcarPonto("Saída");
+        });
 
-function marcarPonto(tipo) {
-    const agora = new Date();
-    const horaAtual = agora.getHours();
-    const minutosAtual = agora.getMinutes();
-    const selecionado = quemEVoceSelect.value;
-    
-    const minutosAtrasados = calcularMinutosAtrasados(horaAtual, minutosAtual);
+        function marcarPonto(tipo) {
+            const agora = new Date();
+            const horaAtual = agora.getHours();
+            const minutosAtual = agora.getMinutes();
+            const selecionado = quemEVoceSelect.value;
+            
+            const minutosAtrasados = calcularMinutosAtrasados(horaAtual, minutosAtual);
 
-    if (tipo === "Entrada") {
-        if (horaAtual === 18 && minutosAtual >= 1 && minutosAtual <= 31) {
-            const taxaDesconto = minutosAtrasados * 0.05;
-            const registro = `${selecionado} bateu o ponto (${tipo}) - ${agora.toLocaleString()}. Atraso de ${minutosAtrasados} minutos. Taxa a ser descontada: $${taxaDesconto.toFixed(2)}`;
-            const registroItem = document.createElement("li");
-            registroItem.textContent = registro;
-            registrosLista.appendChild(registroItem);
-        } else {
-            const registro = `${selecionado} bateu o ponto (${tipo}) - ${agora.toLocaleString()}`;
-            const registroItem = document.createElement("li");
-            registroItem.textContent = registro;
-            registrosLista.appendChild(registroItem);
+            if (tipo === "Entrada") {
+                if (horaAtual === 18 && minutosAtual >= 1 && minutosAtual <= 31) {
+                    const taxaDesconto = minutosAtrasados * 0.05;
+                    const registro = `${selecionado} bateu o ponto (${tipo}) - ${agora.toLocaleString()}. Atraso de ${minutosAtrasados} minutos. Taxa a ser descontada: $${taxaDesconto.toFixed(2)}`;
+                    const registroItem = document.createElement("li");
+                    registroItem.textContent = registro;
+                    registrosLista.appendChild(registroItem);
+                } else {
+                    const registro = `${selecionado} bateu o ponto (${tipo}) - ${agora.toLocaleString()}`;
+                    const registroItem = document.createElement("li");
+                    registroItem.textContent = registro;
+                    registrosLista.appendChild(registroItem);
+                }
+            } else if (tipo === "Saída") {
+                if (horaAtual === 19 && minutosAtual >= 31) {
+                    const taxaFalta = 10.00;
+                    const registro = `${selecionado} bateu o ponto (${tipo}) - ${agora.toLocaleString()}. Levou falta. Taxa de falta: $${taxaFalta.toFixed(2)}`;
+                    const registroItem = document.createElement("li");
+                    registroItem.textContent = registro;
+                    registrosLista.appendChild(registroItem);
+                } else {
+                    const registro = `${selecionado} bateu o ponto (${tipo}) - ${agora.toLocaleString()}`;
+                    const registroItem = document.createElement("li");
+                    registroItem.textContent = registro;
+                    registrosLista.appendChild(registroItem);
+                }
+            }
+
+            // Limpar a pergunta "Quem é você?" após o registro
+            quemEVoceSelect.value = "";
+
+            // Limpar a data e hora atual após o registro
+            dataHoraAtualSpan.textContent = "";
         }
-    } else if (tipo === "Saída") {
-        if (horaAtual === 19 && minutosAtual >= 31) {
-            const taxaFalta = 10.00;
-            const registro = `${selecionado} bateu o ponto (${tipo}) - ${agora.toLocaleString()}. Levou falta. Taxa de falta: $${taxaFalta.toFixed(2)}`;
-            const registroItem = document.createElement("li");
-            registroItem.textContent = registro;
-            registrosLista.appendChild(registroItem);
-        } else {
-            const registro = `${selecionado} bateu o ponto (${tipo}) - ${agora.toLocaleString()}`;
-            const registroItem = document.createElement("li");
-            registroItem.textContent = registro;
-            registrosLista.appendChild(registroItem);
+
+        function calcularMinutosAtrasados(hora, minutos) {
+            if (hora === 18) {
+                return 60 - minutos;
+            } else if (hora > 18) {
+                return (60 - minutos) + ((hora - 19) * 60);
+            } else {
+                return 0;
+            }
         }
-    }
-
-    // Limpar a pergunta "Quem é você?" após o registro
-    quemEVoceSelect.value = "";
-
-    // Limpar a data e hora
